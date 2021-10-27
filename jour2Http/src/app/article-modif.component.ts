3b7590a55388e2c2968c3008dd022f1f53d251ea
Malik H <malik.h@webdevpro.net>
2021-10-27 16:09:29 +0200

    formulaire de modification de l'article concerné

--- jour2Http/src/app/article-modif.component.ts
+++ jour2Http/src/app/article-modif.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { HttpClient } from "@angular/common/http";
-import {Router} from "@angular/router";
+import { Router , ActivatedRoute } from "@angular/router";
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-modif',
   template: `
-    <h1>Ajouter un nouveau article</h1>
+    <h1>Modifier un article</h1>
     <form #add="ngForm" (submit)="onSubmit(add)">
-      <input type="text" name="nom" placeholder="titre article" ngModel #nom="ngModel" >
-      <textarea name="contenu" placeholder="contenu article" ngModel #contenu="ngModel" rows="8"></textarea>
-      <select name="etat" ngModel #etat="ngModel">
+      <input type="hidden" name="id" ngModel #id="ngModel" [value]="article.id" >
+      <input type="text" name="nom" placeholder="titre article" ngModel #nom="ngModel" [value]="article.nom" >
+      <textarea name="contenu" placeholder="contenu article" ngModel #contenu="ngModel" rows="8" [value]="article.contenu"></textarea>
+      <select name="etat" ngModel #etat="ngModel" [value]="article.etat">
         <option value="">veuillez sélectionner un état</option>
         <option value="1" selected="selected">actif</option>
         <option value="0">inActif</option>
@@ -34,18 +36,33 @@ import {Router} from "@angular/router";
 export class ArticleModifComponent implements OnInit {
 
   private urlArticles: string = "http://localhost:3000/articles"
+  public article : any = {}; 
 
-  constructor(private req : HttpClient , private nav : Router) { }
+  constructor(private req : HttpClient , private nav : Router , private route : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap
+    .pipe( 
+      mergeMap( (url) => {
+        const id = url.get("id") as string ;
+        return this.req.get(`${this.urlArticles}/${id}`)
+      })
+    )
+    .subscribe( ( article ) => { 
+      this.article = article 
+      // console.log(article)
+    })
+    // this.req.get()
   }
 
   public onSubmit(add : NgForm){
     if(add.valid){
-      const {nom , contenu , etat} = add.value;
+      console.log(add.value);
+      const {id , nom , contenu , etat} = add.value;
       const profil = localStorage.getItem("auth") as string ;
       const user_id = JSON.parse(profil).id;
-      this.req.post( `${this.urlArticles}` , { 
+      console.log(`${this.urlArticles}/${id}`)
+      this.req.put( `${this.urlArticles}/${id}` , { 
         nom ,
         contenu ,
         etat : etat === "1" ? true : false,

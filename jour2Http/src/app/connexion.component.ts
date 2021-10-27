5f1f8d78028b68cafb7659d10db2076e197cfd1d
Malik H <malik.h@webdevpro.net>
2021-10-27 12:35:41 +0200

    formulaire de connexion

--- jour2Http/src/app/connexion.component.ts
+++ jour2Http/src/app/connexion.component.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { HttpClient } from "@angular/common/http"
 
 @Component({
   selector: 'app-connexion',
   template: `
-    <p>
-      connexion works!
-    </p>
+    <h1>Accéder au back office</h1>
+    <div class="form">
+      <form #connexion="ngForm" (submit)="onSubmit(connexion)">
+        <input type="text" name="login" ngModel #login="ngModel" placeholder="login" required >
+        <input type="password" name="password" ngModel #password="ngModel" placeholder="password" required>
+        <div>
+          <input type="submit">
+        </div>
+      </form>
+    </div>
   `,
   styles: [
+    `.form{
+      min-height:50vh;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+    }
+    form{
+      display:grid;
+      grid-template-columns: repeat(2, 200px);
+      grid-gap: 20px;
+    }
+    input{
+      padding:5px 10px;
+    }
+    form div{
+      grid-column: 1/ -1;
+      text-align:center;
+    }
+    `
   ]
 })
 export class ConnexionComponent implements OnInit {
-
-  constructor() { }
-
+  private urlUsers : string = "http://localhost:3000/users";
+  constructor(private req : HttpClient) { }
   ngOnInit(): void {
+  }
+  public onSubmit( connexion : NgForm){
+    if(connexion.valid){
+      const {login , password }= connexion.value;
+
+      this.req.get( `${this.urlUsers}?login=${login}&password=${password}` )
+      .subscribe( profil => console.log(profil) )
+    }
   }
 
 }

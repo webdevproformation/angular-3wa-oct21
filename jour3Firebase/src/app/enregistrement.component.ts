ca8e5c73647402080d5b1e197b38285226a0a546
Malik H <malik.h@webdevpro.net>
2021-10-28 16:03:20 +0200

    composant permettant de créer un profil user

--- jour3Firebase/src/app/enregistrement.component.ts
+++ jour3Firebase/src/app/enregistrement.component.ts
@@ -3,12 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-enregistrement',
   template: `
-    <p>
-      enregistrement works!
-    </p>
-  `,
-  styles: [
-  ]
+    <h1 class="text-center mt-5">créer un profil utilisateur dans Firebase Auth</h1>
+    <form class="offset-3 col-6">
+      <div class="d-flex my-4">
+        <input name="email" type="email" class="form-control me-3" placeholder="votre@email.fr">
+        <input name="password" type="password" class="form-control" placeholder="votre password">
+      </div>
+      <div class="d-flex justify-content-center">
+        <input type="submit" class="btn btn-outline-dark">
+      </div>
+    </form>
+  `
 })
 export class EnregistrementComponent implements OnInit {
 

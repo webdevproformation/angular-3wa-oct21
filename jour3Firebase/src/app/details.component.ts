c0c8969336ba2f00273cb220b0f4510a84602107
Malik H <malik.h@webdevpro.net>
2021-10-28 13:02:42 +0200

    récupérer les id de chaque opération

--- jour3Firebase/src/app/details.component.ts
+++ jour3Firebase/src/app/details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase }  from "@angular/fire/compat/database"
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-details',
@@ -10,24 +11,34 @@ import { Observable } from "rxjs";
     <form *ngFor="let operation of operations" class="d-flex my-1 align-items-center">
       <input type="text" class="form-control" name="motif" [(ngModel)]="operation.motif" >
       <input type="number" class="form-control mx-3" name="montant" [(ngModel)]="operation.montant">
-      <input type="submit" class="btn btn-danger me-3 btn-sm" value="supp">
-      <input type="submit" class="btn btn-warning btn-sm" value="modif">
+      <input type="submit" class="btn btn-danger me-3 btn-sm" value="supp" (click)="onClickSuppr($event)">
+      <input type="submit" class="btn btn-warning btn-sm" value="modif" (click)="onClickModif($event)">
     <form>
-  `,
-  styles: [
-    `input{
-      height:
-    }`
-  ]
+  `
 })
 export class DetailsComponent implements OnInit {
   public operations : Array<{montant: number , motif : string}> = [];
   constructor(private db : AngularFireDatabase) { }
 
+  public onClickSuppr(id : Event){
+    console.log(id);
+  }
+
+  public onClickModif(id: Event){
+
+  }
+
   ngOnInit(): void { 
-    const obsReq$ = this.db.list("/operations").valueChanges() as Observable<Array<{montant: number , motif : string}>>
-    obsReq$.subscribe( operations => {
-      this.operations = operations; 
+    const obsReq$ = this.db.list("/operations").snapshotChanges();
+    obsReq$
+    .pipe( 
+      map( operations  => { 
+        return { operations.map( ( a : any ) => ({ a : a.key , ...a.payload.val() }) ) }
+      } )
+     )
+    .subscribe( operations => {
+      // this.operations = operations; 
+      console.log(operations);
     } )
 
   }

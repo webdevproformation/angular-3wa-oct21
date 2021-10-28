4b8aada465ff5eed7f58766ee847ffafb10620cd
Malik H <malik.h@webdevpro.net>
2021-10-28 15:49:06 +0200

    composant en cours

--- jour3Firebase/src/app/encours.component.ts
+++ jour3Firebase/src/app/encours.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from "@angular/fire/compat/database"
 
 @Component({
   selector: 'app-encours',
   template: `
-    <p>
-      encours works!
-    </p>
-  `,
-  styles: [
-  ]
+    <h1 class="fs-2">
+      nombre d'action en cours 
+      <span class="badge bg-primary">{{ encours }}</span>
+    </h1>
+  `
 })
 export class EncoursComponent implements OnInit {
 
-  constructor() { }
+  public encours : number = 0
+
+  constructor(private db : AngularFireDatabase) { }
 
   ngOnInit(): void {
+      this.db.list("/todos").valueChanges()
+      .subscribe( (todos :any) => {
+        let encours = 0;
+        todos.forEach( (todo :any) => {
+          if(todo.status === false){
+            encours += 1 
+          }
+        } )
+        this.encours = encours ; 
+      } )
   }
 
 }

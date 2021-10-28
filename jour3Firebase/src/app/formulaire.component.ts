52e25ffe3cf748e549efc7294540105029033a23
Malik H <malik.h@webdevpro.net>
2021-10-28 11:53:44 +0200

    gestion du total global

--- jour3Firebase/src/app/formulaire.component.ts
+++ jour3Firebase/src/app/formulaire.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { AngularFireDatabase }  from "@angular/fire/compat/database"
 
 @Component({
   selector: 'app-formulaire',
   template: `
     <h2>{{ titre }}</h2>
-    <form class="d-flex">
-        <input type="text" placeholder="motif" class="form-control">
-        <input type="number" class="form-control mx-4">
+    <form class="d-flex" #f="ngForm" (submit)="onSubmit(f)">
+        <input type="text" placeholder="motif" class="form-control" name="motif" ngModel #motif="ngModel" required>
+        <input type="number" class="form-control mx-4" name="montant" ngModel #montant="ngModel"  required>
         <input type="submit" class="btn btn-success">
     </form>
-  `,
-  styles: [
-  ]
+  `
 })
 export class FormulaireComponent implements OnInit {
   public titre : string = "ajouter une dépense / recette "
-  constructor() { }
-
+  constructor( private db : AngularFireDatabase ) { }
   ngOnInit(): void {
+  }
+  public onSubmit(f : NgForm){
+    if(f.valid){
+      const operation = f.value ;
+      this.db.list("/operations").push(operation);
+      f.reset();
+    }
   }
 
 }
--- jour3Firebase/src/app/total.component.ts
+++ jour3Firebase/src/app/total.component.ts
@@ -1,20 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase }  from "@angular/fire/compat/database";
+import { reduce , mergeMap , map } from "rxjs/operators" ;
+import { Observable , from } from "rxjs"
+// opérateur permet réduire un tableau => une valeur 
+// tableau qui contient de chiffres => somme de tous les chiffres 
 
 @Component({
   selector: 'app-total',
   template: `
-    <p>
-      total works!
-    </p>
-  `,
-  styles: [
-  ]
+    <h1 class="text-center">Total</h1>
+    <p class="text-center">{{ total }}</p>
+  `
 })
 export class TotalComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private db : AngularFireDatabase) { }
+  public total : number = 0;
   ngOnInit(): void {
+    (this.db.list("/operations").valueChanges() as Observable<Array<{montant: number , motif : string}>>)
+    /* .pipe( 
+      mergeMap( operations => from(operations) ),
+      map( ( operations ) => operations.montant),
+      reduce( (acc  , val :number ) =>  acc + val )
+    ) */
+    .subscribe( 
+      ( operations ) => {
+        let total :number = 0
+        operations.forEach( operation => {
+          total += operation.montant;
+        } )
+        this.total = total;
+    } )
+
   }
 
 }

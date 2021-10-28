4d721c13431ebd9f80045b356d78270223101f12
Malik H <malik.h@webdevpro.net>
2021-10-28 16:04:54 +0200

    composant auth

--- jour3Firebase/src/app/enregistrement.component.ts
+++ jour3Firebase/src/app/enregistrement.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-enregistrement',
@@ -17,7 +18,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnregistrementComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth : AngularFireAuth) { }
 
   ngOnInit(): void {
   }

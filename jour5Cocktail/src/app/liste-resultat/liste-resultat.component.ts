a83886f8844fdfea74c19c05e5b7d29d6595f4a6
Malik H <malik.h@webdevpro.net>
2021-10-29 13:52:52 +0200

    cas pratique

--- jour5Cocktail/src/app/liste-resultat/liste-resultat.component.ts
+++ jour5Cocktail/src/app/liste-resultat/liste-resultat.component.ts
@@ -13,6 +13,19 @@ export class ListeResultatComponent implements OnInit {
   ngOnInit(): void {
     this.cocktail.getCocktail()
     .subscribe( (resultat :any) => console.log(resultat.drinks) )
+    // pouvez afficher dans le composant liste-cocktail
+    /**
+     * afficher sous forme de liste 
+     * <article>
+     *  <h2>strDrink</h2>
+     *  <img => strDrinkThumb:>
+     *  <p></p>strInstructions:
+     *  <ul>
+     *  <li></li> la liste des ingrédients => null => rien si ça contient une string => <li></li>
+     * </ul>
+     * </article>
+     * 
+     */
   }
 
 }
